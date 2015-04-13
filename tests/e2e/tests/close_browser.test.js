87e4c07cf56aa0bdef5b7561d37f4aa32f852796
Daniel Flanagan <daniel@lytedev.io>
2015-04-13 17:17:59 -0500

    added some comments

--- tests/e2e/tests/close_browser.test.js
+++ tests/e2e/tests/close_browser.test.js
@@ -1,3 +1,6 @@
+// This test should ONLY close the webdriver and should (probably)
+// never be modified. Any E2E test suite will have this test
+// injected to close the selenium webdriver.
 test.describe('Close browser', function() {
   test.before(function() {
     e2e.closeBrowser();
--- tests/e2e/tests/start_browser.test.js
+++ tests/e2e/tests/start_browser.test.js
@@ -1,7 +1,11 @@
+// This test should ONLY start the webdriver and should (probably)
+// never be modified. Any E2E test suite will have this test
+// injected to start the selenium webdriver.
 test.describe('Start browser', function() {
   test.before(function() {
     e2e.getBrowser();
   });
+
   test.it('setup selenium browser/driver', function() {
     assert.ok(e2e.driver);
   });
