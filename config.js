c2f460e6bddcd29398b459264db547c1c14df069
Daniel Flanagan <daniel@lytedev.io>
2015-04-16 14:11:25 -0500

    added watch tasks getting built and functioning properly

--- config.js
+++ config.js
@@ -2,6 +2,11 @@ process.env.rootTestUrl = "https://www.emfluence.com";
 
 process.env.environmentType = "production";
 
+process.env.watchTaskFiles = [
+  // Any e2e test files
+  "./tests/e2e/**/*.js"
+];
+
 process.env.defaultBrowser = "phantomjs";
 process.env.baseDriverTimeout = 30000;
 
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
 require('./config.js');
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var path = require('path');
 
 process.env.e2eTestPrefix = "e2e-test-";
 var testPrefix = process.env.e2eTestPrefix;
@@ -16,9 +17,9 @@ var defaultDevSuite = "dev";
 var e2eSuites = {
   "dev": "dev.suite",
   "development": "dev.suite",
-  "prod": "production.suite", 
+  "prod": "production.suite",
   "production": "production.suite",
-  "auth": "auth.suite", 
+  "auth": "auth.suite",
 };
 
 var defaultSuite = process.env.environmentType;
@@ -28,28 +29,60 @@ if (process.env.environmentType === "development") {
 
 // Create E2E gulp tasks from suite index
 var multiBrowserSuite = function(suiteKey) {
-  var phantomjs = function() {
-    process.env.defaultBrowser = "phantomjs";
-    return runE2ESuite(suiteKey);
-  };
-  var firefox = function() {
-    process.env.defaultBrowser = "firefox";
-    return runE2ESuite(suiteKey);
-  };
-  var chrome = function() {
-    process.env.defaultBrowser = "chrome";
-    return runE2ESuite(suiteKey);
-  };
-  // TODO: Add IE, Safari, Mobile, etc.
-  var defaultBrowser = function() {
-    return runE2ESuite(suiteKey);
-  };
-  // TODO: Add watch-style tasks
-  gulp.task(testPrefix + 'phantomjs-' + key, phantomjs);
-  gulp.task(testPrefix + 'firefox-' + key, firefox);
-  gulp.task(testPrefix + 'chrome-' + key, chrome);
-  // TODO: Add IE, Safari, Mobile, etc.
-  gulp.task(testPrefix + key, defaultBrowser);
+  // TODO: Add other browsers
+  var browsers = ["phantomjs", "firefox", "chrome"];
+
+  var createBrowserTask = function(browser) {
+    var taskFunc = function() {
+      process.env.defaultBrowser = browser;
+      return runE2ESuite(suiteKey);
+    };
+    var taskName = testPrefix + browsers[i] + '-' + suiteKey;
+    gulp.task(taskName, taskFunc);
+
+    var watchTaskFunc = function() {
+      process.env.defaultBrowser = browsers[i];
+      var suite = forceLoadSuite(suiteKey);
+      var suites = [];
+      suites.push(suiteKey);
+      if (typeof suite.suitesBefore !== 'undefined') {
+        for (k in suite.suitesBefore) {
+          suites.push(suite.suitesBefore[k]);
+        }
+      }
+      if (typeof suite.suites !== 'undefined') {
+        for (k in suite.suites) {
+          suites.push(suite.suites[k])
+        }
+      }
+      // TODO: Come up with a good way to include specs...
+      var watchFiles = ['./tests/e2e/**/*spec.js'];
+      for (i in suites) {
+        var key = suites[i];
+        watchFiles.push(actualSuiteFile(key));
+        var suite = forceLoadSuite(key);
+        for (k in suite.tests) {
+          watchFiles.push(actualTestFile(suite.tests[k]))
+        }
+      }
+      if (typeof suite.tests !== 'undefined') {
+        for (k in suite.tests) {
+          watchFiles.push(actualTestFile(suite.tests[k]))
+        }
+      }
+      gulp.watch(watchFiles, [taskName]);
+    };
+    gulp.task("watch-" + taskName, [taskName], watchTaskFunc);
+  }
+
+  // Create tasks for browsers
+  for (i in browsers) {
+    createBrowserTask(browsers[i]);
+  }
+
+  // Default browser task and watch version
+  gulp.task(testPrefix + key, [testPrefix + process.env.defaultBrowser + "-" + suiteKey]);
+  gulp.task("watch-" + testPrefix + key, ["watch-" + testPrefix + process.env.defaultBrowser + "-" + suiteKey]);
 };
 
 // For every registered test suite, register the appropriate tasks
@@ -59,40 +92,55 @@ for (key in e2eSuites) {
 
 gulp.task('default', [testPrefix + defaultSuite]);
 
-gulp.task('watch', ['default'], function() {
-  gulp.watch('./tests/**/*.js', ['default']);
-});
+gulp.task('watch', ["watch-" + testPrefix + defaultSuite]);
+
+function actualSuiteFile(suiteKey) {
+  var suiteFile = "";
+  if (suiteKey in e2eSuites) {
+    suiteFile = './tests/e2e/suites/' + e2eSuites[suiteKey] + '.js';
+  } else {
+    suiteFile = './tests/e2e/suites/' + suiteKey + '.js';
+  }
+  return suiteFile;
+}
+
+function actualTestFile(testFile) {
+  var t = testFile;
+  t = "./tests/e2e/tests/" + t;
+  t += ".js";
+  return t;
+}
+
+function forceLoadSuite(suiteKey) {
+  var suiteFile = actualSuiteFile(suiteKey);
+  delete require.cache[path.resolve(suiteFile)];
+  return require(suiteFile);
+}
 
 // Run a single test suite
 function runE2ESuite(suiteKey) {
-  var s = 0;
-  if (suiteKey in e2eSuites) {
-    s = require('./tests/e2e/suites/' + e2eSuites[suiteKey] + ".js");
-  } else {
-    s = require('./tests/e2e/suites/' + suiteKey + ".js");
-  }
-  // Add base classes and browser init/teardown
-  if (typeof s.suitesBefore !== "undefined") {
-    runE2ESuites(s.suitesBefore);
-  }
-  if (typeof s.tests !== "undefined") {
-    for (var i = 0; i < s.tests.length; i++) {
-      s.tests[i] = "./tests/e2e/tests/" + s.tests[i];
-      s.tests[i] += ".js";
+  var suite = forceLoadSuite(suiteKey);
+
+  runE2ESuites(suite.suitesBefore);
+
+  if (typeof suite.tests !== "undefined") {
+    for (var i = 0; i < suite.tests.length; i++) {
+      suite.tests[i] = actualTestFile(suite.tests[i]);
     }
-    s.tests.splice(0, 0, './tests/e2e/index.js');
-    s.tests.splice(1, 0, './tests/e2e/tests/start_browser.test.js');
-    s.tests.push('./tests/e2e/tests/close_browser.test.js');
-    var stream = runE2ETests(s.tests);
+    suite.tests.splice(0, 0, './tests/e2e/index.js');
+    suite.tests.splice(1, 0, './tests/e2e/tests/start_browser.test.js');
+    suite.tests.push('./tests/e2e/tests/close_browser.test.js');
+    var stream = runE2ETests(suite.tests);
   }
-  if (typeof s.suites !== "undefined") {
-    runE2ESuites(s.suites);
-  }
+
+  runE2ESuites(suite.suites);
+
   return stream;
 }
 
 // Run a collection of test suites
 function runE2ESuites(suiteKeys) {
+  if (typeof suiteKeys === 'undefined') return;
   for (i in suiteKeys) {
     return runE2ESuite(suiteKeys[i]);
   }
